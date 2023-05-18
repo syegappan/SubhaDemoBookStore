d27a9be98b579a57633c4fbc992e8917dd2bd80c
Subhashini Yegappan <104956469+syegappan@users.noreply.github.com>
2023-05-17 21:00:17 -0700

    Initialize new-ghas-demo

---
+++ src/main/java/com/github/demo/service/BookDatabase.java
@@ -0,0 +1,15 @@
+package com.github.demo.service;
+
+import com.github.demo.model.Book;
+
+import java.util.List;
+import java.util.Collection;
+
+public interface BookDatabase {
+
+    List<Book> getAll() throws BookServiceException;
+
+    void populate(Collection<Book> books) throws BookServiceException;
+
+    void destroy() throws BookServiceException;
+}
